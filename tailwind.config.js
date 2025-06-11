04586fcf1813de0e1e399b6e76fbe46e50e81451
Devaduth <kmdevaduth@gmail.com>
2025-06-11 13:41:27 +0530

    Mobile view update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,10 @@ module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
     extend: {
-      screens: {
         screens: {
           'tablet': '850px',
           'custom1070':'1070px',
         }
-      }
     },
   },
   plugins: [],
